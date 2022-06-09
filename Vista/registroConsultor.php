2a01f53cc5e8ce725e29ee928a7af99b88d5bb46
mrondonc <mrondonc@unbosque.edu.co>
2022-06-09 18:23:19 -0500

    Update registroConsultor.php

--- Vista/registroConsultor.php
+++ Vista/registroConsultor.php
@@ -58,5 +58,5 @@ if (isset($_POST['addRegistroUsuario']) == true) {
 }
 echo '<script>
     alert("Su cuenta ha sido creada!"); 
-    window.location="/reportRC/Vista/Login.php";          
+    window.location="./Login/index.php";          
     </script>';
